ab29fb80eece4d77aba7b600cf9a3526cafab933
woongaaaa <woonge0930@naver.com>
2025-08-07 16:02:00 +0900

    프로젝트 파일 세팅

---
+++ Memory/Memory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by 황채웅 on 8/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory/Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by 황채웅 on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
